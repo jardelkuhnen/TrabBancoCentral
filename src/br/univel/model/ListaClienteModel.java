0c0c4efa4f949faaf4cabf1eeac1fcaf21df68c5
jardelkuhnen <jardelkuhnen@gmail.com>
2016-08-25 22:01:31 -0300

    Criado classes

---
+++ src/br/univel/model/ListaClienteModel.java
@@ -0,0 +1,24 @@
+package br.univel.model;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ListaClienteModel extends AbstractTableModel {
+
+	@Override
+	public int getColumnCount() {
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/br/univel/view/ListCliente.java
@@ -0,0 +1,24 @@
+package br.univel.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ListCliente extends PadraoBancario {
+
+	private JPanel contentPane;
+
+	public ListCliente() {
+		super();
+		
+		
+		
+		
+		
+	
+	}
+
+}
